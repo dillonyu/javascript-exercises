2e4f4e3a0bb51a106f9eb569ec9ebb60611595f1
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2022-07-05 15:48:04 -0700

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0) {
+        return 'ERROR';
+    }
 
+    /* Method 1: Using a formula */
+    // return (Math.abs(a - b) + 1)/2 * (a + b);
+
+    /* Method 2: Traditional loop method */
+    let index = a;
+    let max = b;
+    let result = 0;
+    if (b < a) {
+        index = b;
+        max = a;
+    }
+    for (; index <= max; index++) {
+        result += index;
+    }
+    return result;
 };
 
 // Do not edit below this line
