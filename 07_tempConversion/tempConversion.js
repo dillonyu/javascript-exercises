d7956b6031a5cdd923510947ad5329080ef4e8d5
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2022-07-05 16:09:26 -0700

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let ans = (f - 32) * 5/9;
+  if (ans.toString().indexOf(".") == -1) {
+    return ans;
+  }
+  return +ans.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let ans = c * 9/5 + 32;
+  if (ans.toString().indexOf(".") == -1) {
+    return ans;
+  }
+  return +ans.toFixed(1);
 };
 
 // Do not edit below this line
