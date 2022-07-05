7e428b82457583351e2c86c1189660d8e570abe4
Dillon Yu <86173315+dillonyu@users.noreply.github.com>
2022-07-05 15:56:47 -0700

    Complete 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (!(year % 4)) {
+        if (year % 100) {
+            return true;
+        }
+        return !(year % 400);
+    }
+    return false;
 };
 
 // Do not edit below this line
